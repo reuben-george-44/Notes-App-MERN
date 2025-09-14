694f349ac0895d2d65cea0c139dc10baefeb89fd
reuben-george-44 <reuben.dev2005@gmail.com>
2025-09-14 22:11:31 +0530

    Adding notes functionality

--- client/src/components/MainPage.jsx
+++ client/src/components/MainPage.jsx
@@ -2,25 +2,31 @@ import { useState } from "react"
 import Note from "./Note"
 
 const MainPage = ({user}) => {
-  const handleSubmit = () => {
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNotes(notes => [...notes, {
+      title: title,
+      desc: desc
+    }])
   }
   const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState(""); 
   return (
-    <div className="fixed inset-0 bg-black font-mate">
+    <div className="fixed inset-0 font-mate">
       <h1 className="text-white">Welcome {user}</h1>
       <div className="absolute top-3 right-3 rounded-full bg-white">
         {/* Hello */}
       </div>
       {
-        notes.map((note) => {
+        notes.map((note) => (
           <Note title={note.title} desc={note.desc}></Note>
-        })
+        ))
       }
-      <form onSubmit={handleSubmit}>
-        <input value={n} type="text" />
-        <input type="text" />
-        <button type="submit"></button>
+      <form name="form" className="min-h-20" onSubmit={handleSubmit}>
+        <input name="title" className="border-2" value={title} onChange={e => setTitle(e.target.value)} type="text" />
+        <input name="desc" className="border-2" value={desc} onChange={e => setDesc(e.target.value)} type="text" />
+        <button className="border-2" type="submit">Submit</button>
       </form>
     </div>
   )
